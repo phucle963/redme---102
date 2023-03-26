71e64a7d016e63bf91ad72af4606cff1cd652b31
le thien phuc <lethienphuc6991@gmail.com>
2023-03-26 18:00:55 +0700

    update js star

--- custom.js
+++ custom.js
@@ -1,1 +1,23 @@
-alert('hi')
+
+function likecokie() {
+let reponse = prompt('Do you like Cake?')
+                
+if (reponse == 'yes') {
+    alert("you're come to the right place!");
+} else if (reponse == 'no') {
+    alert("What are you doing at a shop website");
+} else {
+   arlert("i'm not sure what that means...");
+}
+
+document.write(reponse);
+return reponse;
+}
+
+
+function rateMypage(){
+    let rating = prompt("how many to start would you rate my page? (1-5)");
+    for(i = 0; i < rating; i++){
+    document.write('<img class="star" src="star.png" alt="star">')
+    }
+}
